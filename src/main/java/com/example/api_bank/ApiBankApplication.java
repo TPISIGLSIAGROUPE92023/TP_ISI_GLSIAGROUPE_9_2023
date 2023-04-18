b937a445f9e4dc4239babf8e0356efe24dac6bdb
AuxBasilias <97743681+AuxBasilias@users.noreply.github.com>
2023-04-18 05:00:18 +0000

    structure du projet

---
+++ src/main/java/com/example/api_bank/ApiBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBankApplication.class, args);
+	}
+
+}
